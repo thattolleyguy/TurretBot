6938ab490628e9c3d7b45a5be9c8136cd17c67d4
tyler <tyler@envy-linux>
2014-04-27 08:28:25 -0600

    Remove object files. Change setup for serial input

--- TurretBot/main.cpp
+++ TurretBot/main.cpp
@@ -1,30 +1,68 @@
 #include <Arduino.h>
 #include <AFMotor.h>
+#include <Servo.h>
 
 extern HardwareSerial Serial;
-AF_Stepper panStepper(200, 2);
-int direction=FORWARD;
+//AF_Stepper panStepper(200, 2);
+//AF_Stepper tiltStepper(200, 1);
+//Servo rackServo;
+
+int pos = 0;
+int tiltPin = 3;
+int panPin = 2;
+int style = INTERLEAVE;
+int inputState = 0;
 
 void setup() {
-    panStepper.setSpeed(30);
-    Serial.begin(9600);
+    //    tiltStepper.setSpeed(1);
+    //    panStepper.setSpeed(1);
+    //    rackServo.attach(9);
+    //    rackServo.write(10);
 
+
+    Serial.begin(9600);
 }
-char input;
+
 void loop() {
-    panStepper.step(15,direction, SINGLE);
-    if(Serial.available()>0)
-    {
-        input = Serial.read();
-        if(input=='f')
-        {
-            Serial.write("Changing direction to forward");
-            direction=FORWARD;
-        }
-        if(input=='r')
-        {
-            Serial.write("Changing direction to backward");
-            direction=BACKWARD;
-        }
+    if (Serial.available() > 0) {
+        inputState = Serial.read();
+        Serial.print("Changing input state to:");
+        Serial.println(inputState);
+        Serial.println(inputState & 2);
+
     }
+
+    //Handle input state
+    if ((inputState & 1) != 0) {
+        Serial.println("Tilting Up");
+        //        tiltStepper.onestep(FORWARD,DOUBLE);
+        // Tilt up
+    }
+    if ((inputState & 2) != 0) {
+        Serial.println("Tilting Down");
+        //        tiltStepper.onestep(BACKWARD,DOUBLE);
+        // Tidelt down
+    }
+    if ((inputState & 4) != 0) {
+        Serial.println("Panning Left");
+        //        panStepper.onestep(FORWARD,DOUBLE);
+        // Pan left
+    }
+    if ((inputState & 8) != 0) {
+        Serial.println("Panning Right");
+        //        panStepper.onestep(BACKWARD,DOUBLE);
+        // Pan right
+    }
+    if ((inputState & 16) != 0) {
+        Serial.println("Toggling Flywheel");
+        // Toggle flywheel
+    }
+    if ((inputState & 32) != 0) {
+        Serial.println("Firing");
+        //        rackServo.write(160);
+        //        rackServo.write(10);
+        // Fire
+    }
+
+    delay(500);
 }
